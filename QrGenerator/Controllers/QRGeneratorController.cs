bd1f35b6ff872027b89b29902de0b43c7906275b
koliavashchishin <koliavashchishin@gmail.com>
2023-09-08 14:06:19 +0300

    watermark

--- QrGenerator/Controllers/QRGeneratorController.cs
+++ QrGenerator/Controllers/QRGeneratorController.cs
@@ -9,6 +9,7 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.Collections.Generic;
 using QrGenerator.Attributes;
+using System.Net;
 
 namespace QrGenerator.Controllers
 {
@@ -75,6 +76,8 @@ namespace QrGenerator.Controllers
                         img.ScaleAbsoluteWidth(35);
 
                         pdfContent.AddImage(img);
+
+                        Helpers.AddWaterMark(pdf.watermark, reader, stamper);
                     }
                 }
                 else
@@ -93,6 +96,8 @@ namespace QrGenerator.Controllers
                         img.ScaleAbsoluteWidth(35);
 
                         pdfContent.AddImage(img);
+
+                        Helpers.AddWaterMark(pdf.watermark, reader, stamper);
                     }
 
                 }
@@ -131,154 +136,36 @@ namespace QrGenerator.Controllers
             #endregion
         }
 
-        // Post api/qrgenerator Додає QR у docx документ
-        [HttpPost]
-        public ActionResult<object> AddQRToDocument([FromBody] DocumentQR document)
+        [HttpGet("test")]
+        public ActionResult<object> TestGet()
         {
-            #region Генерація QR коду
-            QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(document.url, QRCodeGenerator.ECCLevel.L);
-            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
-            byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
-            #endregion
-
-            #region Вставка QR коду у документ
-            var bytes = Convert.FromBase64String(document.base64);
-
-            MemoryStream _mem = new MemoryStream(0);
-
-            _mem.Write(bytes, 0, bytes.Length);
-
             try
             {
-                using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(_mem, true))
+                WebRequest request = WebRequest.Create(
+                  "https://docs.microsoft.com");
+                request.Credentials = CredentialCache.DefaultCredentials;
+
+                WebResponse response = request.GetResponse();
+
+                string status;
+                string responseFromServer;
+
+                using (Stream dataStream = response.GetResponseStream())
                 {
-                    MainDocumentPart mainPart = wordDocument.MainDocumentPart;
-
-                    if (document.placement == "end")
-                    {
-                        ImagePart imagePart;
-                        imagePart = mainPart.AddImagePart(ImagePartType.Png);
-                        using (MemoryStream qrStream = new MemoryStream(qrCodeAsPngByteArr))
-                        {
-                            imagePart.FeedData(qrStream);
-                        }
-                        Helpers.AddImageToBody(wordDocument, mainPart.GetIdOfPart(imagePart));
-
-                        // imagePart = GetImagePartByName(wordDocument, "image");
-
-                        //якщо немає QR додємо новий, якщо є оновлюємо старий
-                        // if (imagePart == null)
-                        // {
-                        //   код вище
-                        // }
-                        // else
-                        // {
-                        //     using (MemoryStream qrStream = new MemoryStream(qrCodeAsPngByteArr))
-                        //     {
-                        //         imagePart.FeedData(qrStream);
-                        //     }
-                        // }
-
-                    }
-                    else
-                    {
-                        mainPart.DeleteParts(mainPart.HeaderParts);
-
-                        HeaderPart header = mainPart.AddNewPart<HeaderPart>();
-
-                        string headerPartId = mainPart.GetIdOfPart(header);
-
-                        ImagePart imagePart = header.AddImagePart(ImagePartType.Png);
-                        using (MemoryStream qrStream = new MemoryStream(qrCodeAsPngByteArr))
-                        {
-                            imagePart.FeedData(qrStream);
-                        }
-
-                        Helpers.GenerateHeaderPartContent(header, header.GetIdOfPart(imagePart));
-                        IEnumerable<SectionProperties> sections = mainPart.Document.Body.Elements<SectionProperties>();
-
-                        foreach (var section in sections)
-                        {
-                            section.RemoveAllChildren<HeaderReference>();
-
-                            section.PrependChild<HeaderReference>(new HeaderReference() { Id = headerPartId });
-                        }
-
-                    }
-
-                    wordDocument.Save();
-
-                    wordDocument.Close();
-
-                    Result result = new Result("application/vnd.openxmlformats-officedocument.wordprocessingml.document", Convert.ToBase64String(_mem.ToArray()));
-
-                    return result;
+                    StreamReader reader = new StreamReader(dataStream);
+                    status = ((HttpWebResponse)response).StatusDescription;
+                    responseFromServer = reader.ReadToEnd();
                 }
-                #endregion
 
-                // ImagePart GetImagePartByName(WordprocessingDocument d, string imageName)
-                // {
-                //     try
-                //     {
-                //         return d.MainDocumentPart.ImageParts
-                //             .Where(p => p.Uri.ToString().Contains(imageName))
-                //             .First();
-                //     }
-                //     catch
-                //     {
-                //         return null;
-                //     }
-                // }
+                // Close the response.
+                response.Close();
+
+                return new Result(status, responseFromServer);
             }
             catch (Exception ex)
             {
                 return ex;
             }
-        }
-
-        [HttpPost("identifier")]
-        public ActionResult<object> AddDocumentIdentifier([FromBody] DocumentQR document)
-        {
-
-            #region Вставка QR коду у документ
-            var bytes = Convert.FromBase64String(document.base64);
-
-            MemoryStream _mem = new MemoryStream(0);
-
-            _mem.Write(bytes, 0, bytes.Length);
-
-            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(_mem, true))
-            {
-                var body = wordDoc.MainDocumentPart.Document.Body;
-
-                var paragraphs = body.Elements<DocumentFormat.OpenXml.Wordprocessing.Paragraph>();
-
-                foreach (var para in paragraphs)
-                {
-                    if (para.InnerText.Contains("ДОГОВІР № ____________"))
-                    {
-                        foreach (var run in para.Elements<Run>())
-                        {
-                            foreach (var text in run.Elements<Text>())
-                            {
-                                if (text.Text.Contains("_"))
-                                {
-                                    text.Text = new Random().Next(20000, 99999).ToString();
-                                }
-                            }
-                        }
-                    }
-                }
-
-                wordDoc.Save();
-
-                wordDoc.Close();
-
-                return new Result("application/vnd.openxmlformats-officedocument.wordprocessingml.document", Convert.ToBase64String(_mem.ToArray()));
-            }
-
-            #endregion
         }
     }
 }
